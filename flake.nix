65326bbfd4af8e85af3a14c79e8f7d743865ae5b
Josh Cox <josh@webhosting.coop>
2024-05-26 17:23:10 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake for building clip";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+
+  outputs = {
+    self,
+    nixpkgs
+  }: let
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    suffix-version = version: attrs: nixpkgs.lib.mapAttrs' (name: value: nixpkgs.lib.nameValuePair (name + version) value) attrs;
+    suffix-stable = suffix-version "-23_11";
+  in {
+    packages.x86_64-linux.default = self.packages.x86_64-linux.clip;
+    packages.x86_64-linux.clip =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "clip";
+        src = self;
+        installPhase = "TMP=$(mktemp -d) && cp clip $TMP/clip && chmod 555 $TMP/clip && mkdir -p $out/bin && install -t $out/bin $TMP/clip";
+      };
+  };
+}
